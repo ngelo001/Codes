47ce1e6c74949342e2ad8387ffcb794acf5b3328
ngelo001 <37296803+ngelo001@users.noreply.github.com>
2022-10-13 21:42:13 +0800

    Create get-url-parameter.js

---
+++ Custom jQuery/get-url-parameter.js
@@ -0,0 +1,35 @@
+function getURLParameters(paramName) {
+    var sURL = window.document.URL.toString();
+    if (sURL.indexOf("?") > 0) {
+        var arrParams = sURL.split("?");
+        var arrURLParams = arrParams[1].split("&");
+        var arrParamNames = new Array(arrURLParams.length);
+        var arrParamValues = new Array(arrURLParams.length);
+
+        var i = 0;
+        for (i = 0; i < arrURLParams.length; i++) {
+            var sParam = arrURLParams[i].split("=");
+            arrParamNames[i] = sParam[0];
+            if (sParam[1] != "") arrParamValues[i] = unescape(sParam[1]);
+            else arrParamValues[i] = "No Value";
+        }
+
+        for (i = 0; i < arrURLParams.length; i++) {
+            if (arrParamNames[i] == paramName) {
+                //alert("Parameter:" + arrParamValues[i]);
+                return arrParamValues[i];
+            }
+        }
+        return "No Parameters Found";
+    }
+}
+
+//Sample Usage
+window.addEventListener("load", function () {
+    jQuery(function ($) {
+        $(document).ready(function () {          
+          var portfolio_cat = getURLParameters("portfolio_category");
+          $('option[value="' + portfolio_cat + '"]').prop("selected", true);
+        });
+    });
+});
