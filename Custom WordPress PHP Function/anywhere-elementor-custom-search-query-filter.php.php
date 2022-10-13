94fcd03eafc2b1086ddc8a573329cd0829479dc3
ngelo001 <37296803+ngelo001@users.noreply.github.com>
2022-10-13 22:07:33 +0800

    Update anywhere-elementor-custom-search-query-filter.php.php

--- Custom WordPress PHP Function/anywhere-elementor-custom-search-query-filter.php.php
+++ Custom WordPress PHP Function/anywhere-elementor-custom-search-query-filter.php.php
@@ -1,12 +1,12 @@
 <?php
 function cpt_search_function($query_args){  
-  $booking_search = $_GET['booking_search'] ? $_GET['booking_search'] : '';
-  $query_args =   array   (
-    'post_type' => 'locations',
-    's' => $booking_search,
-  );
+    $booking_search = $_GET['booking_search'] ? $_GET['booking_search'] : '';
+    $query_args =   array   (
+        'post_type' => 'locations',
+        's' => $booking_search,
+    );
 
-  return $query_args;
+    return $query_args;
 }
 add_filter('cpt_query_id', 'cpt_search_function');
 ?>
