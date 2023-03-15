431d291eec84eb86d7aab828eeca08821dbe47db
ngelo001 <37296803+ngelo001@users.noreply.github.com>
2023-03-15 15:01:00 +0800

    Update get-url-parameter.js

--- Custom jQuery/get-url-parameter.js
+++ Custom jQuery/get-url-parameter.js
@@ -1,27 +1,12 @@
 function getURLParameters(paramName) {
     var sURL = window.document.URL.toString();
-    if (sURL.indexOf("?") > 0) {
-        var arrParams = sURL.split("?");
-        var arrURLParams = arrParams[1].split("&");
-        var arrParamNames = new Array(arrURLParams.length);
-        var arrParamValues = new Array(arrURLParams.length);
-
-        var i = 0;
-        for (i = 0; i < arrURLParams.length; i++) {
-            var sParam = arrURLParams[i].split("=");
-            arrParamNames[i] = sParam[0];
-            if (sParam[1] != "") arrParamValues[i] = unescape(sParam[1]);
-            else arrParamValues[i] = "No Value";
-        }
-
-        for (i = 0; i < arrURLParams.length; i++) {
-            if (arrParamNames[i] == paramName) {
-                //alert("Parameter:" + arrParamValues[i]);
-                return arrParamValues[i];
-            }
-        }
-        return "No Parameters Found";
+    if (sURL.indexOf("?") < 0) return "No Parameters Found";
+    var arrParams = sURL.split("?")[1].split("&");
+    for (var i = 0; i < arrParams.length; i++) {
+        var sParam = arrParams[i].split("=");
+        if (sParam[0] == paramName) return sParam[1] ? decodeURIComponent(sParam[1]) : "No Value";
     }
+    return "No Parameters Found";
 }
 
 //Sample Usage
